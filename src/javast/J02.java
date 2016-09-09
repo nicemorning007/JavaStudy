81638c4217db293e80303688efa5239d841f42d4
NiceMorning <oypebook@outlook.com>
2016-09-09 17:24:01 +0800

    T02

--- src/javast/J02.java
+++ src/javast/J02.java
@@ -1,5 +1,22 @@
 package javast;
 
+import java.util.Scanner;
+
 public class J02 {
-	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Try your number:");
+		int x=sc.nextInt();
+		int y=0;
+		if (x<0) {
+			y=x*x-1;
+		}
+		else if (x>0) {
+			y=x+3;
+		}
+		else {
+			;
+		}
+		System.out.println(y);
+	}
 }
