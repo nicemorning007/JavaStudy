a4544accc878ec35a067dbf2784daaf1663293b0
NiceMorning <oypebook@outlook.com>
2016-09-18 12:49:20 +0800

    修正。~(@^_^@)~

--- src/javast/C06_Q01.java
+++ src/javast/C06_Q01.java
@@ -4,21 +4,13 @@ public class C06_Q01 {
 
 	public static void main(String[] args) {
 		String St1 = "HelloWorld";
-		String StrTemp = St1;
-		// char[] Ch1;
-		// Ch1 = St1.toCharArray();
 		StringBuffer Sb1 = new StringBuffer();
 		for (int i = 0; i < St1.length(); i++) {
 			if (Character.isLowerCase(St1.charAt(i))) {
-				StrTemp.toUpperCase();
-				Sb1.append(StrTemp.charAt(i));
+				Sb1.append(Character.toUpperCase(St1.charAt(i)));
 			} else if (Character.isUpperCase(St1.charAt(i))) {
-				StrTemp.toLowerCase();
-				Sb1.append(StrTemp.charAt(i));
-			} else {
-				Sb1.append(StrTemp.charAt(i));
+				Sb1.append(Character.toLowerCase(St1.charAt(i)));
 			}
-			StrTemp = St1;
 		}
 		Sb1.reverse();
 		System.out.println(Sb1.toString());
