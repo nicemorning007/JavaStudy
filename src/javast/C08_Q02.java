c73fde69af4b24b4f0ade0ebf74664d6089609ad
NICEMORNING\oypeb <oypebook@vip.qq.com>
2016-09-23 23:50:16 +0800

    第八章修改完成

--- src/javast/C08_Q02.java
+++ src/javast/C08_Q02.java
@@ -1,44 +1,28 @@
 package javast;
 
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class C08_Q02 {
 
-	public static void main(String[] args) throws IOException {
-		char count;
-		int password;
-		FileReader fileReader = new FileReader("GameGetInTimes.txt");
-		FileWriter fileWriter = new FileWriter("GameGetInTimes.txt");
-		FileReader fileReader2 = new FileReader("PassWord.txt");
-		System.out.println("Please input your password:");
-		if ((count = (char) fileReader.read()) < '4') {
-			while ((password = System.in.read()) != -1) {
-				if (password == (int)fileReader2.read()) {
-					System.out.println("Game Start!");
-					fileReader2.close();
-					fileReader.close();
-					fileWriter.close();
-					System.exit(0);
-				} else {
-					++count;
-					fileWriter.write(count);
-					System.out.println("Password Wrong!!!\nTry again!!!");
-					fileReader2.close();
-					fileReader.close();
-					fileWriter.close();
-					System.exit(0);
-				}
+	public static void main(String[] args) throws Exception {
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+		String password = "";
+		boolean b = false;
+		for (int i = 0; i < 5; i++) {
+			System.out.println("Password:");
+			password = bufferedReader.readLine();
+			if (password.equals("123456")) {
+				System.out.println("Game Start!");
+				b = true;
+				break;
+			} else {
+				System.out.println("Password Error.");
 			}
-		} else {
-			fileReader2.close();
-			fileReader.close();
-			fileWriter.close();
-			System.out
-					.println("You`ve already give the wrong password five times.\nNow you`re not allow to get start.");
-			System.exit(0);
 		}
+		if (!b) {
+			System.out.println("You`re not allow to get start.");
+		}
+		System.exit(0);
 	}
-
 }
